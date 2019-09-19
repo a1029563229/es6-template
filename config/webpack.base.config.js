a26d485859402db02d1951df6db0771f8312b42c
lt <1029563229@qq.com>
2019-09-19 19:10:52 +0800

    debug hotload bug

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -34,7 +34,7 @@ module.exports = {
     },
     plugins: [
         new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
+        // new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({ template: path.join(__dirname, '..', 'index.html') })
     ]
 }
