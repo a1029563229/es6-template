888c4c676da4dbf19427945c599a5843579e457a
lt <1029563229@qq.com>
2019-09-26 15:47:40 +0800

    减少输出

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -16,12 +16,6 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-      {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
